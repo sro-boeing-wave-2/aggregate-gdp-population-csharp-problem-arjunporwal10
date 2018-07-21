dbaf401e7b29c398db5196dda8a578d9c1bb7914
arjun <arjunporwal10@gmail.com>
2018-07-21 13:12:03 +0530

    using sync method

--- AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
+++ AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
@@ -1,5 +1,9 @@
 using System;
 using Xunit;
+using AggregateGDPPopulation;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace AggregateGDPPopulation.Tests
 {
@@ -8,7 +12,22 @@ namespace AggregateGDPPopulation.Tests
         [Fact]
         public void Test1()
         {
-
+            Class1.AggregateGDPPopulation(@"D:\workspace\htmlplaygrnd\C#Assignment\aggregate-gdp-population-csharp-problem-arjunporwal10\AggregateGDPPopulation\data\datafile.csv");
+            string actualFile = File.ReadAllText("../../../expected-output.json");
+            string expectedFile = File.ReadAllText(Environment.CurrentDirectory + @"/output/output.json");
+            Dictionary<string, GDPPopulation> actual = JsonConvert.DeserializeObject<Dictionary<string, GDPPopulation>>(actualFile);
+            Dictionary<string, GDPPopulation> expected = JsonConvert.DeserializeObject<Dictionary<string, GDPPopulation>>(expectedFile);
+            foreach (var key in actual.Keys)
+            {
+                if (expected.ContainsKey(key))
+                {
+                    Assert.Equal(actual[key].GDP_2012, expected[key].GDP_2012);
+                    Assert.Equal(actual[key].POPULATION_2012, expected[key].POPULATION_2012);
+                } else
+                {
+                    Assert.True(false);
+                }
+            }
         }
     }
 }
--- AggregateGDPPopulation/AggregateGDPPopulation.cs
+++ AggregateGDPPopulation/AggregateGDPPopulation.cs
@@ -1,8 +1,66 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace AggregateGDPPopulation
 {
+    public class GDPPopulation
+    {
+        public float GDP_2012 { get; set; }
+        public float POPULATION_2012 { get; set; }
+    }
     public class Class1
     {
+        public static void AggregateGDPPopulation(string FilePath)
+        {
+
+            string[] AllData = File.ReadAllLines(FilePath);
+            StreamReader r = new StreamReader(@"D:\workspace\htmlplaygrnd\C#Assignment\aggregate-gdp-population-csharp-problem-arjunporwal10\countryContinentJsonFile.json");
+            var json = r.ReadToEnd();
+            var Mapper = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+            string[] headers = AllData[0].Split(',');
+            int indexOfPopulation = Array.IndexOf(headers, "\"Population (Millions) 2012\"");
+            int indexOfGDP = Array.IndexOf(headers, "\"GDP Billions (USD) 2012\"");
+            int indexOfCountries = Array.IndexOf(headers, "\"Country Name\"");
+            Dictionary<string, GDPPopulation> objectDictonary = new Dictionary<string, GDPPopulation>();
+            for (int i = 1; i < AllData.Length; i++)
+            {
+                try
+                {
+                    string[] row = AllData[i].Split(',');
+                    string countryName = row[indexOfCountries].Trim('"');
+                    string nameOfContinent = Mapper[countryName];
+                    float Population = float.Parse(row[indexOfPopulation].Trim('"'));
+                    float Gdp = float.Parse(row[indexOfGDP].Trim('"'));
+                    try
+                    {
+
+                        objectDictonary[nameOfContinent].GDP_2012 += Gdp;
+                        objectDictonary[nameOfContinent].POPULATION_2012 += Population;
+
+                    }
+                    catch
+                    {
+                        GDPPopulation Object = new GDPPopulation() { GDP_2012 = Gdp, POPULATION_2012 = Population };
+                        objectDictonary.Add(nameOfContinent, Object);
+                    }
+                }
+                catch { }
+            }
+            var finalObject = JsonConvert.SerializeObject(objectDictonary);
+            string outputFilePath = Environment.CurrentDirectory + @"/output/output.json";
+            Console.WriteLine(outputFilePath);
+            if (!Directory.Exists(Environment.CurrentDirectory + @"/output"))
+            {
+                Directory.CreateDirectory(Environment.CurrentDirectory + @"/output");
+            }
+            StreamWriter outputdata = new StreamWriter(outputFilePath);
+            outputdata.WriteLine(finalObject);
+            outputdata.Close();
+            //Console.WriteLine(finalObject);
+            //Console.ReadLine();
+        }
     }
 }
